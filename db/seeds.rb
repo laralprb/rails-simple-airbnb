9973844621f991f4ff536e34afc6cf5335a5edaa
lara barbosa <larapattylprb@gmail.com>
2021-05-26 18:31:32 -0300

    index/new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating flats..."
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Paris Flat',
+  address: 'Paris',
+  description: 'Beautiful',
+  price_per_night: 90,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Roma Flat',
+  address: 'Roma',
+  description: 'Big flat',
+  price_per_night: 60,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Madrid',
+  address: 'Madrid south',
+  description: 'I can\'t describe',
+  price_per_night: 98,
+  number_of_guests: 5
+)
+
+puts "Finish!"
